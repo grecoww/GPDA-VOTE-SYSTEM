a885158dbe5a8ff09d7a2943a8dd8cbfc4d62056
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-08-06 03:59:34 +0000

    tentativa 6

--- src/app.js
+++ src/app.js
@@ -40,7 +40,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  if (process.env.NODE_ENV === "production") {
+  if (
+    process.env.NODE_ENV === "production" &&
+    req.headers["x-forwarded-proto"] !== "https"
+  ) {
     return res.redirect(`https://${req.headers.host}${req.url}`);
   }
   next();
